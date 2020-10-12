e4915ddbe46d66faf0bfb39d05a126a3f8953376
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-12 04:39:59 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,27 @@
-# Write your code here
+require 'open-uri'
+require 'json'
+require 'net/http'
+
+
+class GetRequester
+  attr_accessor :URL
+  
+  def initialize(url)
+    URL = url
+  end
+  
+  def get_response_body
+    uri = URI.parse(URL)
+    response = NET::HTTP.get_response(uri)
+    response.body
+  end 
+  
+  def parse_json
+    info = JSON.parse(self.get_response_body)
+    info.collect do |item|
+  end 
+  
+  
+  
+  
+end
